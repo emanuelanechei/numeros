4047cc1db878e8d97f0bd06668a94ec83645a4bc
Andres <andres@GizmoAtWork.home>
2020-03-20 19:31:24 +0100

    Merged name and date calculations, pending cycles

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#numerologia.io python code by ΛNDRΞS
+
 #libraries import
 import functools
 import re
+import datetime
 
+#Dictionary
 alpha = {
     'A':1,'Á':1,'Ä':1,  'J':1, 'S':1,'Š':1,
     'B':2, 'K':2, 'T':2, 'Ť':2,
@@ -12,6 +16,15 @@ alpha = {
     'G':7, 'P':7, 'Y':7, 'Ý':7,
     'H':8, 'Q':8, 'Z':8,'Ž':8,
     'I':9,'Í':9, 'Ü':9, 'R':9, 'Ř':9, ' ':' '}
+
+#Function to replace characters in a word with the value
+def cypher(words):
+    result = words.upper()
+    result = [alpha[i] for i in result]
+    strings = [str(x) for x in result]
+    a_string = "".join(strings)
+    # an_integer = int(a_string)
+    return a_string
 
 #Function to check is there's a Master Value
 def master(value):
@@ -29,11 +42,6 @@ def redux(value):
     while int(redux) > 9:
         redux = sum([int(i) for i in str(redux)])
     return redux
-    # if int(redux) > 9:
-    #     redux = sum([int(i) for i in str(redux)])
-    #     return redux
-    # else:
-    #     return redux
 
 #Function to calculate sum of string
  #1721 -> 11 -> 2
@@ -59,18 +67,10 @@ def rem_vowel(string):
 def rem_consonants(string):
     return (re.sub("[qwrtypsdfghjklñzxcvbnmQWRTYPSDFGHJKLÑZXCVBNMčďňřšťžČĎŇŘŠŤŽ]","",string))
 
-#Function to replace characters in a word with the value
-def cypher(words):
-    result = words.upper()
-    result = [alpha[i] for i in result]
-    strings = [str(x) for x in result]
-    a_string = "".join(strings)
-    # an_integer = int(a_string)
-    return a_string
 #Funcion to reduce each string value
 
 def topo(list):
-    for i in list_interior:
+    for i in list:
         result = [redux(i) for i in list]
         return result
 
@@ -86,17 +86,85 @@ def calculin(list):
         # redux = sum([int(i) for i in str(y)])
         return redux
 
-#-----------------USER INPUT----------------------
+#Count characters of string withou  spaces
+def stringcount(string):
+    result = len(string) - string.count(' ')
+    return result
 
+def dotw(date):
+    dotw = date.strftime("%A")
+    return dotw
+
+def realdate(string):
+    day, month, year = map(int, string.split('-'))
+    birth_date = datetime.date(year, month, day)
+    return birth_date
+
+def datecalc(string):
+    string = string.split('-')
+    return string
+
+
+#Cycle funtions
+
+# def cycle(datestring):
+#     if (int(t_month) == 11) or (int(t_month) == 12):
+#         pre_today_year_cycle = int(birth_vibration) + int(calc_sum_t_year) + 1
+#         # print(f" Pre en NOV/DIC: {pre_today_year_cycle}")
+#         today_year_cycle = digitcheck(str(pre_today_year_cycle))
+#         print(f"From October {t_year} to October {t_year_plus}, your YEAR CYCLE is {today_year_cycle}  ")
+#     else:
+#         pre_today_year_cycle = int(birth_vibration) + int(calc_sum_t_year)
+#         # print(f" Pre no -> NOV/DIC: {pre_today_year_cycle}")
+#         today_year_cycle = digitcheck(str(pre_today_year_cycle))
+#         print(f"From October {t_year_minus} to October {t_year}, your YEAR CYCLE is {today_year_cycle}  ")
+#     pass
+
+
+
+#-----------------USER INPUT----------------------
+#----------------------
+#----------------------
+
+#Welcome
+print()
+print("-- -- -- -- -- -- -- -- -- --")
+print("Bienvenido a numerología.io")
+print("-- -- -- -- -- -- -- -- -- --")
+print()
 
 input_full_name = input("Type your full name: ")
-name_count = len(input_full_name) - input_full_name.count(' ')
+date_input = input("Escribe tu fecha de nacimiento en el formato: DD-MM-YYYY (sin Zeros ""0""): ")
+birth_date = realdate(date_input)
+birth_dotw = dotw(birth_date)
+
+#Birth Date calculations
+#----------------------
+v_year = str(birth_date.year)
+v_month = str(birth_date.strftime('%m'))
+v_day = str(birth_date.strftime('%d'))
+#----------------------
+
+redux_year = redux(v_year)
+redux_month = redux(v_month)
+redux_day = redux(v_day)
+
+date_string = str(redux_year)+str(redux_month)+str(redux_day)
+birth_vibration = calsum(date_string)
+
+name_count = stringcount(input_full_name)
 interior_values = rem_consonants(input_full_name)
 exterior_values = rem_vowel(input_full_name)
-# interior = calsum(interior_values)
-# exterior = calsum(exterior_values)
 
-# calc = cypher(input_full_name)
+# choice = input("Do you want to consult a specific date?? (y/n): ")
+# choice = choice.upper()
+# if choice == 'Y':
+#     cross = PrintCross(person)
+#     cross.printCross()
+# else:
+#     pass
+
+
 calc_name = cypher(input_full_name)
 calc_interior = cypher(interior_values)
 calc_exterior = cypher(exterior_values)
@@ -111,15 +179,14 @@ intext = interior[0] + exterior[0]
 # goals = sum([int(i) for i in str(calc_goals)])
 goals = calsum(str(intext))
 
-# number = numerology(input_full_name)
-# input_number = input("Type numbers: ")
-# sum = digit_sum(input_number)
+
 
 #Results
 print()
 print("-- Resultados de consulta --")
-print(input_full_name)
-print(f" Your name contains {name_count} letters")
+print()
+print(f"Your name is: {input_full_name}")
+print(f"Your name contains {name_count} letters")
 print(calc_name)
 print()
 print(interior_redux)
@@ -128,8 +195,22 @@ print(input_full_name)
 print(list_exterior)
 print(exterior_redux)
 print()
-print(f" Your INTERIOR is {interior}")
-print(f" Your EXTERIOR is {exterior}")
+print(f"Your INTERIOR is {interior}")
+print(f"Your EXTERIOR is {exterior}")
 # print(f" Your GOAL is {calc_goals}")
-print(f" Your GOAL is {goals}")
-# print(sum)
+print(f"Your GOAL is {goals}")
+print()
+print("-- Resultados de Fecha --")
+print()
+print(f"Your birth date is: {birth_date}")
+print(f"You where born on a: {birth_dotw}")
+# print(type(birth_date))
+# print(type(date_list))
+# print(v_year)
+# print(v_month)
+# print(v_day)
+# print(redux_year)
+# print(redux_month)
+# print(redux_day)
+# print(date_string)
+print(f"Your Vibration is: {birth_vibration}")
--- num.py
+++ num.py
@@ -1,35 +1,56 @@
-#libraries import
-import functools
-import re
+import datetime
 
-def master(value):
-    number = int(value)
-    if number == 11 or number == 22 or number == 33 or number == 44:
-        ismaster = True
-        return ismaster
-    else:  # Numbers 11, 22, 33 or 44 should not be reduced.
-        ismaster = False
-        return ismaster
+class Person:
+    def __init__(self, full_name, birth_date):
+        self.full_name = full_name
+        self.birth_date = birth_date
+        self.alpha = {
+            'A':1,'Á':1,'Ä':1,  'J':1, 'S':1,'Š':1,
+            'B':2, 'K':2, 'T':2, 'Ť':2,
+            'C':3, 'Č':3, 'Ç':3, 'L':3, 'U':3, 'Ú':3, 'Ů':3,
+            'D':4, 'Ď':4, 'M':4, 'V':4,
+            'E':5, 'É':5, 'Ě':5,'N':5,'Ň':5, 'W':5,'Ö':5,
+            'F':6, 'O':6,'Ó':6, 'X':6,
+            'G':7, 'P':7, 'Y':7, 'Ý':7,
+            'H':8, 'Q':8, 'Z':8,'Ž':8,
+            'I':9,'Í':9, 'Ü':9, 'R':9, 'Ř':9, ' ':' '}
 
- #1721 -> 11 -> 2
-def calsum(value):
-    if master(value) == True:
-        master_value = sum([int(i) for i in str(value)])
-        return [master_value, True, value]
-    else:
-        suma  = sum([int(i) for i in value])
-        if master(suma) == True:
-            master_value = sum([int(i) for i in str(suma)])
-            return [master_value, True, suma]
-        else:
-            final = sum([int(i) for i in str(suma)])
-            return [final, False, 0]
+    def dotw(self):
+        dotw = self.birth_date.strftime("%A")
+        return dotw
+
+    def master(value):
+        number = int(value)
+        if number == 11 or number == 22 or number == 33 or number == 44:
+            ismaster = True
+            return ismaster
+        else:  # Numbers 11, 22, 33 or 44 should not be reduced.
+            ismaster = False
+            return ismaster
 
 
 
 
-input_number = input("Type numbers: ")
 
-# master = master(input_number)
-sum = calsum(input_number)
-print(sum)
+#Welcome
+print("Bienvenido a numerología.io")
+print()
+
+#Birth Date input
+#----------------------
+input_full_name = input("Escribe tu nombre completo (cómo en el registro civil): ")
+date_entry = input("Escribe tu fecha de nacimiento en el formato: DD-MM-YYYY: ")
+#Date calculations
+day, month, year = map(int, date_entry.split('-'))
+birth_date = datetime.date(year, month, day)
+dotw_birth = person.dotw()
+
+person = Person(input_full_name, birth_date)
+print(person.dotw())
+
+print(day)
+print(month)
+print(year)
+print(birth_date)
+print(f" Nombre introducido es: {person.full_name}")
+print(f" Tu fecha de nacimiento es: {person.birth_date}")
